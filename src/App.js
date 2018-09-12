e21cb0a8485bc4b2eec39474e14c602545809ead
Wickerman2 <christian_wickerstrom@hotmail.com>
2018-09-12 22:38:19 +0200

    mapping clicked keys to generated tones.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import Tone from 'tone';
+import Keyboard from './components/Keyboard.js'
 
 class App extends Component {
   constructor(props) {
     super(props)
-
-    let synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
   };
 
   
@@ -15,20 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <ul class="set">
-            <li class="white b"></li>
-            <li class="black as"></li>
-            <li class="white a"></li>
-            <li class="black gs"></li>
-            <li class="white g"></li>
-            <li class="black fs"></li>
-            <li class="white f"></li>
-            <li class="white e"></li>
-              <li class="black ds"></li>
-              <li class="white d"></li>
-              <li class="black cs"></li>
-              <li class="white c"></li>
-          </ul>
+        <Keyboard />
       </div>
     );
   }
